eab44302a9a7d2d334ec9c7733c5763e14e5807d
Serhii Diuh <sdiuh@isaca.org>
2023-07-20 14:47:37 +0300

    Initial commit

---
+++ SimpleDocument/Program.cs
@@ -0,0 +1,27 @@
+﻿/* ================================================================
+ * Author: Tony Qu 
+ * Author's email: tonyqus (at) gmail.com 
+ * NPOI Examples: https://github.com/nissl-lab/npoi-examples
+ * ==============================================================*/
+
+using NPOI.XWPF.UserModel;
+using System.IO;
+using System.IO.Compression;
+
+namespace SimpleDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XWPFDocument();
+
+            doc.CreateTOC();
+
+            using (var fs = new FileStream("simple.docx", FileMode.Create))
+            {
+                doc.Write(fs);
+            }
+        }
+    }
+}
---
+++ SimpleDocumentBroken/Program.cs
@@ -0,0 +1,34 @@
+﻿/* ================================================================
+ * Author: Tony Qu 
+ * Author's email: tonyqus (at) gmail.com 
+ * NPOI Examples: https://github.com/nissl-lab/npoi-examples
+ * ==============================================================*/
+
+using NPOI.XWPF.UserModel;
+using System.IO;
+using System.IO.Compression;
+
+namespace SimpleDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XWPFDocument();
+
+            doc.CreateTOC();
+
+            using (var zipStream = new FileStream("test.zip", FileMode.Create))
+            {
+                using (var zipArchive = new ZipArchive(zipStream, ZipArchiveMode.Create))
+                {
+                    var entry = zipArchive.CreateEntry($"simple-broken.docx", CompressionLevel.Optimal);
+                    using (var entryStream = entry.Open())
+                    {
+                        doc.Write(entryStream);
+                    }
+                }
+            }
+        }
+    }
+}
